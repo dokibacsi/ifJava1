5b166a2c45735ba3be4961c74f0cf5ab3a0baf3c
dokibacsi <kvalcsi02@gmail.com>
2023-12-04 08:53:25 +0100

    Hasznalat osztalyban tobbi peldanyositva

--- src/interfacegyak/Hasznalat.java
+++ src/interfacegyak/Hasznalat.java
@@ -1,4 +1,11 @@
 package interfacegyak;
 public class Hasznalat {
+    Ajto ajto = new Ajto();
+    Esernyo esernyo = new Esernyo();
+    Konzerv konzerv = new Konzerv();
     
+    Nyithato nyithato = new Nyithato()
+    {
+    
+    };
 }
